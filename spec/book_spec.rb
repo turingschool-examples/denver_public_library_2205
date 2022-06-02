6f634d73c9ad2b0f303335e8d07d8725e32f4172
John Santosuosso <john.santosuosso1@gmail.com>
2022-06-01 21:53:59 -0400

    initial commit

---
+++ spec/book_spec.rb
@@ -0,0 +1,28 @@
+require './lib/book'
+
+RSpec.describe Book do
+  before(:each) do
+    @book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+  end
+
+  it 'exists' do
+    expect(@book).to be_an_instance_of(Book)
+  end
+
+  it 'has an author first_name' do
+    expect(@book.author_first_name).to eql("Harper")
+  end
+
+  it 'has an author last_name' do
+    expect(@book.author_last_name).to eql("Lee")
+  end
+
+  it 'has a title' do
+    expect(@book.title).to eql("To Kill a Mockingbird")
+  end
+
+  it 'has a publication date' do
+    expect(@book.publication_date).to eql("July 11, 1960")
+  end
+
+end
