99b5c8f4c5a4e307319b6e1e8b045e2069c9057d
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-06-02 09:20:21 -0500

    test: library specs

---
+++ spec/library_spec.rb
@@ -0,0 +1,76 @@
+require './lib/library'
+require './lib/author'
+require 'pry'
+
+RSpec.describe Library do
+  it "is a Library" do
+    dpl = Library.new("Denver Public Library")
+
+    expect(dpl).to be_instance_of(Library)
+  end
+
+  xit "has a name" do
+    dpl = Library.new("Denver Public Library")
+
+    expect(dpl.name).to eq("Denver Public Library")
+  end
+
+  xit "has no books by default" do
+    dpl = Library.new("Denver Public Library")
+
+    expect(dpl.books).to eq([])
+  end
+
+  xit "has no authors by default" do
+    dpl = Library.new("Denver Public Library")
+
+    expect(dpl.authors).to eq([])
+  end
+
+  xit "can add authors" do
+    dpl = Library.new("Denver Public Library")
+
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+
+    dpl.add_author(charlotte_bronte)
+    dpl.add_author(harper_lee)
+
+    expect(dpl.authors).to eq([charlotte_bronte, jane_eyre])
+  end
+
+  xit "can provide a list of all Books in the Library" do
+    dpl = Library.new("Denver Public Library")
+
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    professor = charlotte_bronte.write("The Professor", "1857")
+    villette = charlotte_bronte.write("Villette", "1853")
+
+    harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+    mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+
+    dpl.add_author(charlotte_bronte)
+    dpl.add_author(harper_lee)
+
+    expect(dpl.books).to eq([jane_eyre, professor, villette, mockingbird])
+  end
+
+  xit "can give a publication time frame for Authors" do
+    dpl = Library.new("Denver Public Library")
+
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    professor = charlotte_bronte.write("The Professor", "1857")
+    villette = charlotte_bronte.write("Villette", "1853")
+
+    harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+    mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+
+    dpl.add_author(charlotte_bronte)
+    dpl.add_author(harper_lee)
+
+    expect(dpl.publication_time_frame_for(charlotte_bronte)).to eq({:start=>"1847", :end=>"1857"})
+    expect(dpl.publication_time_frame_for(harper_lee)).to eq({:start=>"1960", :end=>"1960"})
+  end
+end
