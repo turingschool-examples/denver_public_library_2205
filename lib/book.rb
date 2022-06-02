28ac9363eca462682f2a0b4167c9e4f20554df42
Eli Sachs <easachs13@gmail.com>
2022-06-01 19:04:26 -0600

    Add checkout_count to Book class

--- lib/book.rb
+++ lib/book.rb
@@ -3,13 +3,15 @@ class Book
   attr_reader :author_first_name,
               :author_last_name,
               :title,
-              :publication_date
+              :publication_date,
+              :checkout_count
 
   def initialize(data)
     @author_first_name = data[:author_first_name]
     @author_last_name = data[:author_last_name]
     @title = data[:title]
     @publication_date = data[:publication_date]
+    @checkout_count = 0
   end
 
   def author
@@ -18,6 +20,10 @@ class Book
 
   def publication_year
     @publication_date[-4..-1]
+  end
+
+  def add_checkout
+    @checkout_count += 1
   end
 
 end
