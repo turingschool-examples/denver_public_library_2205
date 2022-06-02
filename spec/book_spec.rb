8784984e7fc36372bd84007871e01b3331ab2a2e
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-06-02 08:46:47 -0500

    tests: book spec tests

---
+++ spec/book_spec.rb
@@ -0,0 +1,28 @@
+require './lib/book'
+require 'binding.pry'
+
+RSpec.describe Book do
+  it "is a Book" do
+    book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+
+    expect(book).to be_instance_of(Book)
+  end
+
+  it "has a title" do
+    book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+
+    expect(book.title).to eq("To Kill a Mockingbird")
+  end
+
+  it "has an author" do 
+    book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+
+    expect(book.author).to eq("Harper Lee")
+  end
+
+  it "has a publication year" do
+    book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+
+    expect(book.publication_year).to eq("1960")
+  end
+end
