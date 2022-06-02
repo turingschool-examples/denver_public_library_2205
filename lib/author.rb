a1ca4d7d133e457b0a30fba25057815bbd939b6a
John Santosuosso <john.santosuosso1@gmail.com>
2022-06-01 22:12:29 -0400

    initialize author class

---
+++ lib/author.rb
@@ -0,0 +1,11 @@
+require_relative 'book'
+
+class Author
+  attr_reader :first_name, :last_name
+
+  def initialize(attributes)
+    @first_name = attributes[:first_name]
+    @last_name = attributes[:last_name]
+  end
+
+end
