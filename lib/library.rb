7e2e905413b2af345caae3ad3ece89364492830a
Eli Sachs <easachs13@gmail.com>
2022-06-01 19:04:02 -0600

    Update Library spec checkout/return

--- lib/library.rb
+++ lib/library.rb
@@ -5,12 +5,14 @@ class Library
 
   attr_reader :name,
               :books,
-              :authors
+              :authors,
+              :checked_out_books
 
   def initialize(name)
     @name = name
     @books = []
     @authors = []
+    @checked_out_books = []
   end
 
   def add_author(author)
@@ -30,6 +32,27 @@ class Library
     timeframe_hash[:start] = sorted_books.first.publication_year
     timeframe_hash[:end] = sorted_books.last.publication_year
     timeframe_hash
+  end
+
+  def checkout(book)
+    valid = @books.include?(book) && !@checked_out_books.include?(book)
+    if valid
+      @checked_out_books << book
+      book.add_checkout
+    end
+    return valid
+  end
+
+  def return(book)
+    @checked_out_books.delete_if do |checked_out_book|
+      checked_out_book.title == book.title
+    end
+  end
+
+  def most_popular_book
+    @books.sort_by do |book|
+      book.checkout_count
+    end.last
   end
 
 end
