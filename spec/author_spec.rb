9b45b42a404c87ea33fcf0ad5f5fcd8deb64ae8b
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-06-02 09:01:15 -0500

    test: Author specs

---
+++ spec/author_spec.rb
@@ -0,0 +1,42 @@
+require './lib/book'
+require './lib/author'
+require 'pry'
+
+RSpec.describe Author do
+  it "is an Author" do
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+
+    expect(charlotte_bronte).to be_instance_of(Author)
+  end
+
+  xit "has a name" do
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+
+    expect(charlotte_bronte.name).to eq("Charlotte Bronte")
+  end
+
+  xit "has no books by default" do
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+
+    expect(charlotte_bronte.books).to eq([])
+  end
+
+  xit "can write a Book" do
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+
+    expect(jane_eyre).to be_instance_of(Book)
+    expect(jane_eyre.title).to eq("Jane Eyre")
+  end
+
+  xit "stores books it writes" do
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    villette = charlotte_bronte.write("Villette", "1853")
+
+    expect(charlotte_bronte.books.count).to eq(2)
+    expect(charlotte_bronte.books).to eq([jane_eyre, villette])
+  end
+end
