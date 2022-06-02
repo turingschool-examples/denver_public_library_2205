1b83b769efb5fdd9ef537ed515e5ca010059e9b9
Eli Sachs <easachs13@gmail.com>
2022-06-01 18:41:48 -0600

    Update require for Library spec; write tests for It 4

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -1,6 +1,4 @@
 require './lib/library'
-require './lib/author'
-require './lib/book'
 
 RSpec.describe Library do
 
@@ -54,5 +52,56 @@ RSpec.describe Library do
       expect(dpl.publication_time_frame_for(charlotte_bronte)[:end]).to eq("1857")
       expect(dpl.publication_time_frame_for(harper_lee)).to eq({start: "1960", end: "1960"})
     end
+  end
+
+  describe "#iteration 4" do
+
+    dpl = Library.new("Denver Public Library")
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    villette = charlotte_bronte.write("Villette", "1853")
+    harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+    mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+
+    xit "can't checkout nonexistent books" do
+      expect(dpl.checkout(mockingbird)).to be_false
+      expect(dpl.checkout(jane_eyre)).to be_false
+    end
+
+    xit "can checkout added author books" do
+      expect(dpl.checkout(jane_eyre)).to be_true
+    end
+
+    xit "has checked out books array" do
+      expect(dpl.checked_out_books).to be_a(Array)
+      expect(dpl.checked_out_books.first.title).to eq("Jane Eyre")
+    end
+
+    xit "can't checkout currently checked out books" do
+      expect(dpl.checkout(jane_eyre)).to be_false
+    end
+
+    xit "can return a book" do
+      dpl.return(jane_eyre)
+      expect(checked_out_books).to eq([])
+    end
+
+    xit "can hold multiple checked out books" do
+      expect(dpl.checkout(jane_eyre)).to be_true
+      expect(dpl.checkout(villette)).to be_true
+      expect(dpl.checked_out_books.length).to eq(2)
+      expect(dpl.checked_out_books.last.title).to eq("Villette")
+    end
+
+    xit "knows most popular book" do
+      dpl.checkout(mockingbird)
+      dpl.return(mockingbird)
+      dpl.checkout(mockingbird)
+      dpl.return(mockingbird)
+      dpl.checkout(mockingbird)
+      expect(dpl.most_popular_book).to be_a(Book)
+      expect(dpl.most_popular_book.title).to eq("To Kill a Mockingbird")
+    end
+
   end
 end
