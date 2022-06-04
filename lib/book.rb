c105fb0101948ef8642356df0a6de61616249771
Jonathan Pope <jonathanmpope@gmail.com>
2022-06-04 07:55:10 -0600

    Feat: add publication year feat

--- lib/book.rb
+++ lib/book.rb
@@ -1,9 +1,11 @@
+require 'pry'
 class Book
-  attr_reader :title, :author 
+  attr_reader :title, :author, :publication_year
 
   def initialize(info)
     @title = info[:title]
     @author = "#{info[:author_first_name]} #{info[:author_last_name]}"
+    @publication_year = "#{info[:publication_date]}"[-4..-1]
   end
 
 end
--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -5,21 +5,19 @@ RSpec.describe do
     @book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
   end
 
-  it "is an instance of" do
+  it 'is an instance of' do
     expect(@book).to be_instance_of Book
   end
 
-  it "has a title" do
+  it 'has a title' do
     expect(@book.title).to eq("To Kill a Mockingbird")
   end
 
-  it "has an author" do
+  it 'has an author' do
     expect(@book.author).to eq("Harper Lee")
   end
 
-  xit "has a publication year" do
-
+  it 'has a publication year' do
+    expect(@book.publication_year).to eq("1960")
   end
-
-
 end
