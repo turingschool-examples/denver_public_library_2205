20fd2f3d1691b5c30ba5f7470fa04b1ad983afaa
Ethan Nguyen <etnguyen16@gmail.com>
2022-06-05 16:28:19 -0600

    define publication year and add to inititalize

--- lib/book.rb
+++ lib/book.rb
@@ -7,6 +7,11 @@ class Book
     @title = info[:title]
     @author = info[:author_first_name] + " " + info[:author_last_name]
     @publication_date = info[:publication_date]
+    @publication_year = info[:publication_date][-4..-1]
+  end
+
+  def publication_year
+    @publication_year
   end
 
 end
