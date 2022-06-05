e0c02abe361d611e802abee8ecb0d4d5d7560ddc
alepbloyd <alexanderleeboyd@gmail.com>
2022-06-04 21:34:24 -0400

    Test: author exists

---
+++ spec/author_spec.rb
@@ -0,0 +1,18 @@
+require 'rspec'
+require './lib/book'
+require './lib/author'
+
+RSpec.describe Author do
+
+  before :each do
+    @charlotte_bronte = Author.new({
+      first_name: "Charlotte",
+      last_name: "Bronte"
+      })
+  end
+
+  it 'exists' do
+    expect(@charlotte_bronte).to be_instance_of(Author)
+  end
+
+end
