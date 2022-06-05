7d5658ffc17a56def7454332857b4cdbd58a01e5
alepbloyd <alexanderleeboyd@gmail.com>
2022-06-04 21:39:35 -0400

    Feat: books array

--- lib/author.rb
+++ lib/author.rb
@@ -1,11 +1,13 @@
 class Author
 
   attr_reader :first_name,
-              :last_name
+              :last_name,
+              :books
 
   def initialize(input)
     @first_name = input[:first_name]
     @last_name = input[:last_name]
+    @books = []
   end
 
   def name
