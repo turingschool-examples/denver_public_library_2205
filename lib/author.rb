fd0592ee0a8dbc3c4d805251f6c128ed22344938
Blake Saylor <blakesaylor@gmail.com>
2022-06-05 13:24:20 -0600

    Add write method to pass tests

--- lib/author.rb
+++ lib/author.rb
@@ -1,10 +1,22 @@
+require './lib/book'
+
 class Author
-  attr_reader :first_name, :last_name, :name, :books
+  attr_reader :books, :first_name, :last_name, :name
 
   def initialize(attributes)
     @first_name = attributes[:first_name]
     @last_name = attributes[:last_name]
     @name = @first_name + ' ' + @last_name
     @books = []
+  end
+
+  def write(book_title, book_publication_date)
+    new_book = Book.new({
+      author_first_name: @first_name,
+      author_last_name: @last_name,
+      title: book_title,
+      publication_date: book_publication_date})
+    @books << new_book
+    new_book
   end
 end
