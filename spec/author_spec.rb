841e2802feb57d4d2663e36a5d96ffba871a33bd
alepbloyd <alexanderleeboyd@gmail.com>
2022-06-04 21:38:59 -0400

    Test: returns empty books array

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -19,4 +19,8 @@ RSpec.describe Author do
     expect(@charlotte_bronte.name).to eq("Charlotte Bronte")
   end
 
+  it 'initializes with empty array of books' do
+    expect(@charlotte_bronte.books).to eq([])
+  end
+
 end
