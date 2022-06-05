13668072c7f565824f60420cf33300fcb79fa81b
alepbloyd <alexanderleeboyd@gmail.com>
2022-06-04 21:20:56 -0400

    Feat: initialize book

---
+++ lib/book.rb
@@ -0,0 +1,10 @@
+class Book
+
+  def initialize(input)
+    @author_first_name = input[:author_first_name]
+    @author_last_name = input[:author_last_name]
+    @title = input[:title]
+    @publication_date = input[:publication_date]
+  end
+
+end
