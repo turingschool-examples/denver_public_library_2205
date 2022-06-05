3268104e8006bc02ae21c0b81bb6583c9164dc60
Blake Saylor <blakesaylor@gmail.com>
2022-06-05 13:47:45 -0600

    Create publication_time_frame to pass tests

--- lib/library.rb
+++ lib/library.rb
@@ -9,4 +9,22 @@ class Library
     @books = []
     @authors = []
   end
+
+  def add_author(author)
+    @authors << author
+    author.books.each do |book|
+      @books << book
+    end
+  end
+
+  def publication_time_frame_for(author)
+    publishing_years = []
+    author.books.each do |book|
+      publishing_years << book.publication_year.to_i
+    end
+
+    start_year = publishing_years.sort.first
+    end_year = publishing_years.sort.last
+    output_hash = {:start => start_year.to_s, :end => end_year.to_s}
+  end
 end
