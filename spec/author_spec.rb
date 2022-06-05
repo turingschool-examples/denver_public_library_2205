5a525c0f855342fd86d1d94ee8ce37f93e15a877
John Santosuosso <john.santosuosso1@gmail.com>
2022-06-05 16:25:05 -0400

    update author and book specs

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -9,7 +9,19 @@ RSpec.describe Author do
   it 'exists' do
     expect(@charlotte_bronte).to be_an_instance_of(Author)
   end
-  
 
+  it 'has a name' do
+    expect(@charlotte_bronte.name).to eql("Charlotte Bronte")
+  end
+
+  it 'writes a book' do
+    jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    expect(jane_eyre.class).to eql(Book)
+  end
+
+  it 'has a title' do
+    jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    expect(jane_eyre.title).to eql("Jane Eyre")
+  end
 
 end
--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -9,22 +9,10 @@ RSpec.describe Book do
     expect(@book).to be_an_instance_of(Book)
   end
 
-  it 'has an author first_name' do
-    expect(@book.author_first_name).to eql("Harper")
-  end
-
-  it 'has an author last_name' do
-    expect(@book.author_last_name).to eql("Lee")
-  end
-
   it 'has a title' do
     expect(@book.title).to eql("To Kill a Mockingbird")
   end
-
-  it 'has a publication date' do
-    expect(@book.publication_date).to eql("July 11, 1960")
-  end
-
+  
   it 'has an author' do
     expect(@book.author).to eql("Harper Lee")
   end
