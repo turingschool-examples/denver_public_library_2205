c90cb229e97c5d8bd990cf048f0954fcb989a8c6
alepbloyd <alexanderleeboyd@gmail.com>
2022-06-04 21:51:13 -0400

    Test: library exists

---
+++ spec/library_spec.rb
@@ -0,0 +1,16 @@
+require 'rspec'
+require './lib/book'
+require './lib/author'
+require './lib/library'
+
+RSpec.describe Library do
+
+  before :each do
+    @dpl = Library.new("Denver Public Library")
+  end
+
+  it 'exists' do
+    expect(@dpl).to be_instance_of(Library)
+  end
+
+end
