682ec78e778f6615886e4665e78deea6e6ab4b06
alepbloyd <alexanderleeboyd@gmail.com>
2022-06-04 21:17:55 -0400

    Test: Book class exists

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,1 +1,16 @@
 require 'rspec'
+require './lib/book'
+
+RSpec.describe Book do
+
+  it 'exists' do
+    book = Book.new({
+      author_first_name: "Harper",
+      author_last_name: "Lee",
+      title: "To Kill a Mockingbird",
+      publication_date: "July 11, 1960"})
+
+    expect(book).to be_instance_of(Book)
+  end
+
+end
