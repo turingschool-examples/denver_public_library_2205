c473eedc3ef54108e0f192716d642754fa6fca55
mkbonini <mkbonini@gmail.com>
2022-06-05 14:08:45 -0600

    Add book class with iteration 1 methods

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -2,23 +2,23 @@ require './lib/book'
 
 RSpec.describe Book do
   before(:each) do
-    book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+    @book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
   end
-  decribe 'Iteration 1' do
+  describe 'Iteration 1' do
     it "exists" do
-      expect(book).to be_a(Book)
+      expect(@book).to be_a(Book)
     end
 
     it 'has a title' do
-      expect(book.title).to eq("To Kill a Mockingbird")
+      expect(@book.title).to eq("To Kill a Mockingbird")
     end
 
     it 'has an author' do
-      expect(book.author).to eq("Harper Lee")
+      expect(@book.author).to eq("Harper Lee")
     end
 
     it 'has a published year' do
-      expect(book.published_year).to eq("1960")
+      expect(@book.publication_year).to eq("1960")
     end
   end
 end
