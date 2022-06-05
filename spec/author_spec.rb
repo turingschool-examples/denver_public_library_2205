e45eed3e923d1884ab8c7f148338da703a22d638
mkbonini <mkbonini@gmail.com>
2022-06-05 14:36:34 -0600

    Add tests for library class for iteration 3

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -3,7 +3,6 @@ require './lib/author'
 
 RSpec.describe Author do
   before(:each) do
-    # @book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
     @author = Author.new({first_name: "Charlotte", last_name: "Bronte"})
   end
   describe 'Iteration 2' do
