394742b1ee3b7dd1657212a41ab2b00a36e09d3c
Tyler Caudill <teecaud@hotmail.com>
2022-06-04 21:34:57 -0500

    feat/ Author. methods = write, add_book)

--- lib/author.rb
+++ lib/author.rb
@@ -1,8 +1,21 @@
 class Author
 
+  attr_reader :first_name, :last_name, :full_name, :books
   def initialize(info)
     @first_name = info[:first_name]
     @last_name = info[:last_name]
-
+    @full_name = "#{@first_name} #{@last_name}"
+    @books = []
   end
+
+  def write(title, publication_date)
+        Book.new({:title => title, :publication_date => publication_date})
+  end
+
+  def add_book(book)
+    @books << book
+  end
+
+
+
 end
--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -1,5 +1,6 @@
 require './lib/book'
 require './lib/author'
+require 'pry'
 
 RSpec.describe Author do
   it "exists" do
@@ -8,6 +9,45 @@ RSpec.describe Author do
   expect(charlotte_bronte).to be_instance_of Author
   end
 
+  it "has a name" do
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+
+    expect(charlotte_bronte.full_name).to eq("Charlotte Bronte")
+  end
+
+  it "books has an empty array by default" do
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+
+    expect(charlotte_bronte.books).to eq([])
+  end
+
+  it "can write books" do
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+
+
+    expect(jane_eyre).to be_instance_of Book
+    expect(jane_eyre.class).to eq(Book)
+    expect(jane_eyre.title).to eq("Jane Eyre")
+
+    villette = charlotte_bronte.write("Villette", "1853")
+
+    expect(villette).to be_instance_of Book
+    expect(villette.class).to eq(Book)
+    expect(villette.title).to eq("Villette")
+    expect(villette.publication_year).to eq("1853")
+  end
+
+  it "can add books to an author" do
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    villette = charlotte_bronte.write("Villette", "1853")
+
+    charlotte_bronte.add_book(jane_eyre)
+    charlotte_bronte.add_book(villette)
+
+    expect(charlotte_bronte.books).to eq([jane_eyre, villette])
 
   end
+
 end
