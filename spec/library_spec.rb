63d5189d108dc504623623c6d4a543a60cc8a60d
alepbloyd <alexanderleeboyd@gmail.com>
2022-06-04 21:59:58 -0400

    Fix: add authors/books to before block

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -7,6 +7,18 @@ RSpec.describe Library do
 
   before :each do
     @dpl = Library.new("Denver Public Library")
+
+    @charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+
+    @jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+
+    @professor = @charlotte_bronte.write("The Professor", "1857")
+
+    @villette = @charlotte_bronte.write("Villette", "1853")
+
+    @harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+
+    mockingbird = @harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
   end
 
   it 'exists' do
