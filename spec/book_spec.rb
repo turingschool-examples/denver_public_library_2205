32d15ca87a4fe92f3a93309ba738407c6b8578ab
Ethan Nguyen <etnguyen16@gmail.com>
2022-06-05 16:14:56 -0600

    add author test and fix syntax for readability

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -2,7 +2,7 @@ require './lib/book'
 require 'pry'
 
 RSpec.describe Book do
-  before do
+  before :each do
 
   @book = Book.new({author_first_name: "Harper",
                     author_last_name: "Lee",
@@ -17,6 +17,11 @@ RSpec.describe Book do
   it "has a title" do
     expect(@book.title).to eq("To Kill a Mockingbird")
   end
+
+  it "has an author" do
+    expect(@book.author).to eq("Harper Lee")
+  end
+
 
 
 
