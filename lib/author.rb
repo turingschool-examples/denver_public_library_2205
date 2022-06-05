cb862d8c530ed8a33d92bbea6e1ec228666af201
John Santosuosso <john.santosuosso1@gmail.com>
2022-06-05 16:25:32 -0400

    create methods for book and author classes through write method

--- lib/author.rb
+++ lib/author.rb
@@ -1,11 +1,21 @@
 require_relative 'book'
 
 class Author
-  attr_reader :first_name, :last_name
+  attr_reader :name, :books
 
   def initialize(attributes)
-    @first_name = attributes[:first_name]
-    @last_name = attributes[:last_name]
+    @name = attributes[:first_name] + " " + attributes[:last_name]
+    @books = []
   end
 
+  def write(title, year)
+    author_first = @name.split(" ").first
+    author_last = @name.split(" ").last
+    attributes = {author_first_name: author_first, author_last_name: author_last, title: title, publication_date: year}
+    book = Book.new(attributes)
+  end
+
+
 end
+
+require 'pry'; binding.pry
--- lib/book.rb
+++ lib/book.rb
@@ -1,19 +1,11 @@
+require_relative 'author'
 class Book
-  attr_reader :author_first_name, :author_last_name, :title, :publication_date
+  attr_reader :author, :title, :publication_year
 
   def initialize(attributes)
-    @author_first_name = attributes[:author_first_name]
-    @author_last_name = attributes[:author_last_name]
+    @author = attributes[:author_first_name] + " " + attributes[:author_last_name]
     @title = attributes[:title]
-    @publication_date = attributes[:publication_date]
-  end
-
-  def author
-    @author_first_name.concat(" ", @author_last_name)
-  end
-
-  def publication_year
-    @publication_date.split.last
+    @publication_year = attributes[:publication_date].split.last
   end
 
 
