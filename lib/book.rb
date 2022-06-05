0ecab6dd697cd4d672a4330595547f4886c34287
Tyler Caudill <teecaud@hotmail.com>
2022-06-04 20:35:32 -0500

    feat Book class w/ tests

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,14 @@
 class Book
 
+  attr_reader :author_first_name, :author_last_name, :title, :publication_date, :author_full_name, :publication_year
+
   def initialize(info)
-    
+    @author_first_name = info[:author_first_name]
+    @author_last_name = info[:author_last_name]
+    @title = info[:title]
+    @publication_date = info[:publication_date]
+    @author_full_name = "#{@author_first_name} #{@author_last_name}"
+    @publication_year = info[:publication_date][-4..-1]
+
   end
 end
--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -6,6 +6,24 @@ RSpec.describe Book do
     it "exists" do
       book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
 
-      expect(book).to be_instance_of Book 
-  end
+      expect(book).to be_instance_of Book
+    end
+
+    it "has a title" do
+      book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+
+      expect(book.title).to eq("To Kill a Mockingbird")
+    end
+
+    it "has an author" do
+      book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+
+      expect(book.author_full_name).to eq("Harper Lee")
+    end
+
+    it "has a publication_year" do
+      book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+
+      expect(book.publication_year).to eq("1960")
+    end
 end
