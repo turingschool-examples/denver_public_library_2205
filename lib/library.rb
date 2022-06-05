d6775f7326cc4efbdcb1e688159fc139a62f195f
alepbloyd <alexanderleeboyd@gmail.com>
2022-06-04 22:16:34 -0400

    Feat: publication_time_frame_for

--- lib/library.rb
+++ lib/library.rb
@@ -17,4 +17,15 @@ class Library
     end
   end
 
+  def publication_time_frame_for(author)
+    return_hash = {}
+    book_dates = []
+    author.books.each do |book|
+      book_dates << book.publication_year.to_i
+    end
+    return_hash[:start] = (book_dates.min).to_s
+    return_hash[:end] = (book_dates.max).to_s
+    return_hash
+  end
+
 end
