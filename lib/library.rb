ff858833287d0f7ec99c027e1bc5bbe5c8f5bf66
Anna Marie Sterling <sterling.am@gmail.com>
2022-06-05 21:42:38 -0600

    "Completed iteration 3

--- lib/library.rb
+++ lib/library.rb
@@ -1,13 +1,33 @@
 class Library
-  attr_reader :name, :books, :authors
+  attr_reader :name, :authors, :books
 
   def initialize(name)
     @name = name
     @books = []
     @authors = []
   end
-  
+
   def add_author(author)
     @authors << author
+  end
+
+  def add_book(book)
+    @books << book
+  end
+
+  def publication_time_frame_for(writer)
+    time_frame = {}
+    years = []
+      authors.each do |author|
+        if author == writer
+          author.books.select do |book|
+            years << book.publication_year
+        end
+        years.minmax
+        time_frame[:start] = years.minmax.first
+        time_frame[:end] = years.minmax.last
+      end
+    end
+    time_frame
   end
 end
