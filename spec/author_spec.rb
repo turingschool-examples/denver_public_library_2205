d269e923e93cebb5f4580dacfb28a02ca9690bbc
Bryce Simonds <bsimonds14@gmail.com>
2022-06-05 18:48:17 -0600

    Test: author exists

---
+++ spec/author_spec.rb
@@ -0,0 +1,16 @@
+require './lib/book'
+require './lib/author'
+
+RSpec.describe Author do
+before :each do
+  @charlotte_bronte = Author.new({
+    first_name: "Charlotte",
+    last_name: "Bronte"
+    })
+  end
+
+  it "exists" do
+    expect(@charlotte_bronte).to be_a(Author)
+  end
+
+end
