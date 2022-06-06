15a970f90c9147ba8f2eb618cf918dba6faad9f5
Thiago Silveira <thiago1992@yahoo.com>
2022-06-06 11:59:57 -0600

    Feat: Pass test book has attributes

--- lib/book.rb
+++ lib/book.rb
@@ -1,3 +1,17 @@
 class Book
-  
+  attr_reader :title,
+              :author_first_name,
+              :author_last_name,
+              :author,
+              :publication_date,
+              :publication_year
+
+  def initialize(info)
+    @title = info[:title]
+    @author_first_name = info[:author_first_name]
+    @author_last_name  = info[:author_last_name]
+    @author            = author_first_name + " " + author_last_name
+    @publication_date  = info[:publication_date]
+    @publication_year  = publication_date[-4..-1]
+  end
 end
