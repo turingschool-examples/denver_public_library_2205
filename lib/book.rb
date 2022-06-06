f1c3b5ecb7ef5b87593b2874a2c209f1396acd29
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-06-06 15:18:34 -0700

    Feat/updated initialize method with book attributes and added attr_reader for title/author/publication_year

--- lib/book.rb
+++ lib/book.rb
@@ -1,8 +1,15 @@
 class Book
+  attr_reader :title,
+              :author,
+              :publication_year
 
   def initialize(attributes)
-    @attributes = attributes
+    @author_first_name = attributes[:author_first_name]
+    @author_last_name = attributes[:author_last_name]
+    @title = attributes[:title]
+    @publication_date = attributes[:publication_date]
+    @author = @author_first_name + " " + @author_last_name
+    @publication_year = @publication_date.split.last
   end
-
 
 end
