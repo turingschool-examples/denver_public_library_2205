723ccebdcb2b75cdb55596d091dd5e84c78820a8
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-06-06 16:06:35 -0700

    Test/added attributes test

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -6,7 +6,13 @@ RSpec.describe Library do
   let(:dpl) { Library.new("Denver Public Library") }
 
   it "exists" do
-    expect(dpl).to be_a(Library)  
+    expect(dpl).to be_a(Library)
+  end
+
+  it "has attributes" do
+    expect(dpl.name).to eq("Denver Public Library")
+    expect(dpl.books).to eq([])
+    expect(dpl.authors).to eq([])
   end
 
 end
