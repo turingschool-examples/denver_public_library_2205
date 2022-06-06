85dc00fe6e60335cd6e90016cbd4e054372c16d8
Thiago Silveira <thiago1992@yahoo.com>
2022-06-06 12:42:44 -0600

    Test: Library has attributes

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -6,4 +6,11 @@ RSpec.describe Library do
     dpl = Library.new("Denver Public Library")
     expect(dpl).to be_a(Library)
   end
+
+  it 'has attributes' do
+    dpl = Library.new("Denver Public Library")
+    expect(dpl.name).to eq("Denver Public Library")
+    expect(dpl.books).to eq([])
+    expect(dpl.authors).to eq([])
+  end
 end
