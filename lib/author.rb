2fad41dd3af2cfbb04bef25357579d65e7bbc800
Bryce Simonds <bsimonds14@gmail.com>
2022-06-05 19:06:05 -0600

    Feat: has book attribute

--- lib/author.rb
+++ lib/author.rb
@@ -1,13 +1,15 @@
 class Author
 attr_reader :author_full_name,
             :first_name,
-            :last_name
+            :last_name,
+            :books
 
 
   def initialize(details)
     @first_name = details[:first_name]
     @last_name = details[:last_name]
     @author_full_name = "#{details[:first_name]} " + "#{details[:last_name]}"
+    @books = []
   end
 
 end
