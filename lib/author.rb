54382a73641f87f2b7e6571ed3b28c326c13562f
Lucas Colwell <colwellc37@gmail.com>
2022-06-06 16:51:22 -0600

    Feat: create write method

--- lib/author.rb
+++ lib/author.rb
@@ -1,8 +1,21 @@
+require 'book'
+
 class Author
-  attr_reader :,
-              :
+  attr_reader :books
 
   def initialize(data)
-    @argument = argument
+    @first_name = data[:first_name]
+    @last_name = data[:last_name]
+    @books = []
+  end
+
+  def name
+    @first_name + " " + @last_name
+  end
+
+  def write(name, date)
+    new_book = Book.new({author_first_name: @first_name, author_last_name: @last_name, title: name, publication_date: date})
+    @books << new_book
+    return new_book
   end
 end
