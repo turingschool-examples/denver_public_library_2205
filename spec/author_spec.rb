c7008e204e57e8cf3f327da8b71e106cbb00b43a
Nick Jones <nickjones9605@gmail.com>
2022-06-05 21:12:06 -0600

    Test: add test for returning if a author exists and if you can return the details of an author

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -5,8 +5,16 @@ require './lib/author.rb'
 RSpec.describe Author do
 
     it "exists" do
-      author = Author.new
+      data = {first_name: "Charlotte",
+       last_name: "Bronte"}
+      author = Author.new(data)
       expect(author).to be_instance_of(Author)
     end
-    
+
+    it "return the detail of an author" do
+      data = {first_name: "Charlotte",
+       last_name: "Bronte"}
+      charlotte_bronte = Author.new(data)
+       expect(charlotte_bronte.name).to eq("Charlotte Bronte")
+    end
 end
