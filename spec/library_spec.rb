a3aa52bccd1d269602c79ca2b7a66bdd0cac9752
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-06-06 16:18:19 -0700

    Test/can authors and return an array test

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -4,6 +4,8 @@ require './lib/author'
 RSpec.describe Library do
 
   let(:dpl) { Library.new("Denver Public Library") }
+  let(:charlotte_bronte) { Author.new({first_name: "Charlotte", last_name: "Bronte"}) }
+  let(:harper_lee) { Author.new({first_name: "Harper", last_name: "Lee"}) }
 
   it "exists" do
     expect(dpl).to be_a(Library)
@@ -13,6 +15,13 @@ RSpec.describe Library do
     expect(dpl.name).to eq("Denver Public Library")
     expect(dpl.books).to eq([])
     expect(dpl.authors).to eq([])
+  end
+
+  it "can add authors and return an array" do
+    dpl.add_author(charlotte_bronte)
+    dpl.add_author(harper_lee)
+
+    expect(dpl.authors).to eq([charlotte_bronte, harper_lee])
   end
 
 end
