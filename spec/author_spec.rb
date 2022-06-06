470e18b8b8a503fb5cbc68a3943fa916fc20b926
Thiago Silveira <thiago1992@yahoo.com>
2022-06-06 12:15:09 -0600

    Test: Author can write a book and return books wrote

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -12,4 +12,18 @@ RSpec.describe Author do
     expect(charlotte_bronte.name).to eq("Charlotte Bronte")
     expect(charlotte_bronte.books).to eq([])
   end
+
+  it 'can write a book and return books wrote' do
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    expect(jane_eyre.class).to eq(Book)
+    expect(jane_eyre.title).to eq("Jane Eyre")
+
+    villette = charlotte_bronte.write("Villette", "1853")
+    expect(villette.class).to eq(Book)
+    expect(villette.title).to eq("Villette")
+
+    expect(charlotte_bronte.books).to eq([jane_eyre, villette])
+  end
 end
