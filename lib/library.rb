aba1c898db0bd49624582e9b1d4ecaeb5f6a4aab
Brenna Stuart <brennacodes@gmail.com>
2022-06-06 12:56:35 -0600

    Test: Library has books count failing

--- lib/library.rb
+++ lib/library.rb
@@ -9,4 +9,20 @@ class Library
     @authors = []
   end
 
+  def add_author(author_name)
+    @authors << author_name
+    add_authors_books
+  end
+
+  def add_authors_books
+    @authors.each {|author| author.books.each {|book| @books << book}}
+  end
+
+  def publication_time_frame_for(author_name)
+    pull_author = @authors.select do |author|
+      author.name == author_name
+    end
+    pull_author.books.each {|book| book.publication_date}.minmax
+    require "pry"; binding.pry
+  end
 end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -2,6 +2,12 @@ require './lib/helper'
 
 RSpec.describe Library do
   let!(:dpl) {Library.new("Denver Public Library")}
+  let!(:charlotte_bronte) {Author.new({first_name: "Charlotte", last_name: "Bronte"})}
+  let!(:harper_lee) {Author.new({first_name: "Harper", last_name: "Lee"})}
+  let!(:new_book1) {charlotte_bronte.write("Jane Eyre", "October 16, 1847")}
+  let!(:new_book2) {charlotte_bronte.write("The Professor", "1857")}
+  let!(:new_book3) {charlotte_bronte.write("Villette", "1853")}
+  let!(:new_book4) {harper_lee.write("To Kill a Mockingbird", "July 11, 1960")}
 
   it "exists" do
     expect(dpl).to be_instance_of Library
@@ -17,5 +23,25 @@ RSpec.describe Library do
 
   it "has authors" do
     expect(dpl.authors).to eq([])
+  end
+
+  it "has books" do
+    expect(dpl.books).to eq([])
+    dpl.add_author(charlotte_bronte)
+    dpl.add_author(harper_lee)
+    expect(dpl.books.first).to be_instance_of Book
+    expect(dpl.books.count).to eq(4)
+  end
+
+  it "has authors" do
+    expect(dpl.books).to eq([])
+    charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    charlotte_bronte.write("The Professor", "1857")
+    charlotte_bronte.write("Villette", "1853")
+    harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+    dpl.add_author(charlotte_bronte)
+    dpl.add_author(harper_lee)
+    expect(dpl.authors[0]).to be_instance_of Author
+    expect(dpl.authors.count).to eq(2)
   end
 end
