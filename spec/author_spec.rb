1d57f830fd0780b9b76bfc821f576ee244667080
Bryce Simonds <bsimonds14@gmail.com>
2022-06-05 19:09:26 -0600

    Test: the write method

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -21,4 +21,15 @@ before :each do
     expect(@charlotte_bronte.books).to eq([])
   end
 
+  it "can add books to the books array attribute" do
+    @jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+
+    expect(@jane_eyre.class).to be_a(Book)
+    expect(@jane_eyre.title).to eq("Jane Eyre")
+    expect(@jane_eyre.books).to eq([@jane_eyre])
+
+    @villette = @charlotte_bronte.write("Villette", "1853")
+    expect(@charlotte_bronte.books).to eq([@jane_eyre])
+  end
+
 end
