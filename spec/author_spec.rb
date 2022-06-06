5caeb16fc976cf93844c1eb57ac0442ee869de60
Nick Jones <nickjones9605@gmail.com>
2022-06-05 21:24:07 -0600

    Test: add test for if author can write a book

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -6,15 +6,31 @@ RSpec.describe Author do
 
     it "exists" do
       data = {first_name: "Charlotte",
-       last_name: "Bronte"}
+              last_name: "Bronte"}
       author = Author.new(data)
       expect(author).to be_instance_of(Author)
     end
 
-    it "return the detail of an author" do
+    it "return the details of an author" do
       data = {first_name: "Charlotte",
-       last_name: "Bronte"}
+              last_name: "Bronte"}
       charlotte_bronte = Author.new(data)
-       expect(charlotte_bronte.name).to eq("Charlotte Bronte")
+      expect(charlotte_bronte.name).to eq("Charlotte Bronte")
+      expect(charlotte_bronte.first_name).to eq("Charlotte")
+      expect(charlotte_bronte.last_name).to eq("Bronte")
+      expect(charlotte_bronte.books).to eq([])
+    end
+
+    it "can test if an author can write a book and return the author's books" do
+      data = {first_name: "Charlotte",
+              last_name: "Bronte"}
+      charlotte_bronte = Author.new(data)
+      expect(charlotte_bronte.books).to eq([])
+      jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+      expect(jane_eyre.class).to be_instance_of(Book)
+      expect(jane_eyre.title).to eq("Jane Eyre")
+      expect(charlotte_bronte.books.length).to eq(1)
+      villette = charlotte_bronte.write("Villette", "1853")
+      expect(charlotte_bronte.books.length).to eq(2)
     end
 end
