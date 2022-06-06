69b6d8778d3ff86f3eb8829910faab0fe9cbe827
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-06-06 17:26:06 -0600

    feat: completed iteration 2 with working tests

--- lib/author.rb
+++ lib/author.rb
@@ -1,11 +1,20 @@
+require './lib/book'
 class Author
 
-  attr_reader :first_name, :last_name, :name
+  attr_reader :first_name, :last_name, :name, :books, :new_book, :author_first_name, :author_last_name, :title, :publication_date
 
   def initialize(input)
     @first_name = input[:first_name]
     @last_name = input[:last_name]
     @name = @first_name + " " + @last_name
+    @books = []
+  end
+
+  def write(book_name, date_written)
+    @new_book = Book.new({author_first_name: @first_name, author_last_name: @last_name, title: book_name, publication_date: date_written})
+    @books << @new_book
+
+    return @new_book
   end
 
 end
