91743a971dbd8abd2d30e2d8d04e708cc64cc77f
Bryce Simonds <bsimonds14@gmail.com>
2022-06-05 18:37:02 -0600

    Feat: initialize method is complete

--- lib/book.rb
+++ lib/book.rb
@@ -2,12 +2,14 @@ class Book
 attr_reader :title,
             :author,
             :author_first_name,
-            :author_last_name
+            :author_last_name,
+            :publication_date
 
   def initialize(details)
-    @title = details[:title],
-    @author = details[:author]
+    @title = details[:title]
     @author_first_name = details[:author_first_name]
     @author_last_name = details[:author_last_name]
+    @publication_date = details[:publication_date]
+    @author = "#{details[:author_first_name]} " + "#{details[:author_last_name]}"
   end
 end
