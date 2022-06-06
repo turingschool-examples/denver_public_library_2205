bd4dca1a81a3f1a8da230553f55ec4d427c641d2
Bryce Simonds <bsimonds14@gmail.com>
2022-06-05 19:30:57 -0600

    Feat: write method complete

--- lib/author.rb
+++ lib/author.rb
@@ -1,3 +1,4 @@
+require './lib/book'
 class Author
 attr_reader :author_full_name,
             :first_name,
@@ -10,6 +11,18 @@ attr_reader :author_full_name,
     @last_name = details[:last_name]
     @author_full_name = "#{details[:first_name]} " + "#{details[:last_name]}"
     @books = []
+  end
+
+  def write(book_title, publish_year)
+    new_book = Book.new({
+        author_first_name: @first_name,
+        author_last_name: @last_name,
+        author_full_name: @author_full_name,
+        title: book_title,
+        publication_date: publish_year
+        })
+        @books << new_book
+        new_book
   end
 
 end
