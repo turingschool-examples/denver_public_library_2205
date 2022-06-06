889e0673af28055fa916e20b4a0571fce7dd51c2
Mary Turpin <maryvirginiaturpin@gmail.com>
2022-06-05 23:40:50 -0600

    create files and first test

---
+++ spec/book_spec.rb
@@ -0,0 +1,9 @@
+require './lib/book'
+
+RSpec.describe Book do
+  it 'exists' do
+    book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+
+    expect(book).to be_a(Book)
+  end
+end
