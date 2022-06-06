87794090d06b1c1f2c162104ceaf17a62cf4ce15
Ethan Nguyen <etnguyen16@gmail.com>
2022-06-05 18:30:05 -0600

    add write test and write method

--- lib/author.rb
+++ lib/author.rb
@@ -1,18 +1,39 @@
 require "pry"
+require './lib/book'
 
 class Author
   attr_reader :first_name,
   :last_name,
-  :name
+  :name,
+  :books
 
   def initialize(info)
     @first_name = info[:first_name]
     @last_name = info[:last_name]
     @name = name
+    @books = []
   end
 
   def name
     @name = @first_name + " " + @last_name
+  end
+
+  # def books(book)
+  #   @books << book
+  #   # i am confused on why this doesn't work
+  # i ended up skipping it, moving on, add it in a different method, and it worked.
+  # end
+
+  def write(title, publication_date)
+    book = Book.new({
+      author_first_name: @name.split(" ")[0],
+      author_last_name: @name.split(" ")[1],
+      title: title,
+      publication_date: publication_date
+    })
+
+    @books << book
+    return book
   end
 
 
--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -22,5 +22,25 @@ RSpec.describe Author do
     expect(@charlotte_bronte.books).to eq([])
   end
 
+  #managed to skip this ^ and came back 
+
+  it "can write a Book and stores" do
+    @charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+
+    @jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+
+    expect(@jane_eyre).to be_instance_of(Book)
+    expect(@jane_eyre.title).to eq("Jane Eyre")
+
+    @charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+
+    @jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    @villette = @charlotte_bronte.write("Villette", "1853")
+
+    expect(@charlotte_bronte.books.count).to eq(2)
+    expect(@charlotte_bronte.books).to eq([@jane_eyre, @villette])
+  end
+
+
 
 end
