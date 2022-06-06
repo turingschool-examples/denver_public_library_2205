17566bc29790a7a83e451177934894a276260f11
Ethan Nguyen <etnguyen16@gmail.com>
2022-06-05 19:02:02 -0600

    add last test for iteration 3

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -35,4 +35,14 @@ RSpec.describe Library do
       expect(@dpl.authors).to eq([@charlotte_bronte, @harper_lee])
       expect(@dpl.books).to eq([@jane_eyre, @professor, @villette, @mockingbird])
     end
+
+    it "has publication time" do
+
+      expect(@dpl.publication_time_frame_for(@charlotte_bronte)).to eq({:start => "1847",
+                                                                        :end => "1857"
+                                                                        })
+      expect(@dpl.publication_time_frame_for(@harper_lee)).to eq({:start => "1960",
+                                                                  :end => "1960"
+                                                                  })
+    end
 end
