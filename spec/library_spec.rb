79d1ea9009834cec201f030a7ca36115133d62ac
Bryce Simonds <bsimonds14@gmail.com>
2022-06-06 13:39:43 -0600

    Test: has details and this passed

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -11,4 +11,10 @@ before :each do
     expect(@dpl).to be_a(Library)
   end
 
+  it "has details" do
+    expect(@dpl.name).to eq("Denver Public Library")
+    expect(@dpl.books).to eq([])
+    expect(@dpl.authors).to eq([])
+  end
+
 end
