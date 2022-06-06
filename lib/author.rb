26002acf29de91a268fce433a381e7373f9669cc
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-06-06 15:52:47 -0700

    Feat/wrote code for write method

--- lib/author.rb
+++ lib/author.rb
@@ -8,4 +8,10 @@ class Author
     @name = @first_name + " " + @last_name
     @books = []
   end
+  
+  def write(title, publication_date)
+    book = Book.new( {author_first_name: @first_name, author_last_name: @last_name, title: title, publication_date: publication_date })
+    @books << book
+    return book
+  end
 end
