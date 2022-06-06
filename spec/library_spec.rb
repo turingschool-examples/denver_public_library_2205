7de045b50937781259236f03d4cebda490de03e0
Jared Hardinger <jaredhardinger@gmail.com>
2022-06-06 12:36:18 -0400

    Refactor

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -35,13 +35,13 @@ RSpec.describe Library do
     expect(@dpl.publication_time_frame_for(harper_lee)).to eq({:start=>"1960", :end=>"1960"})
   end
 
-  Describe 'iteration 4' do
+  describe 'iteration 4:' do
     before :each do
       @charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
-      @jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
-      @villette = charlotte_bronte.write("Villette", "1853")
+      @jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+      @villette = @charlotte_bronte.write("Villette", "1853")
       @harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
-      @mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+      @mockingbird = @harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
     end
 
     it 'can checkout books' do
@@ -50,14 +50,15 @@ RSpec.describe Library do
 
       @dpl.add_author(@charlotte_bronte)
       @dpl.add_author(@harper_lee)
-
       @dpl.checkout(@jane_eyre)
+      
       expect(@dpl.checked_out_books).to eq([@jane_eyre])
     end
 
     it 'cannot check out already checked out books' do
       @dpl.add_author(@charlotte_bronte)
       @dpl.add_author(@harper_lee)
+      @dpl.checkout(@jane_eyre)
 
       expect(@dpl.checkout(@jane_eyre)).to be(false)
     end
