c48fe796957ac92fdbbf312124c4f0d292cfeeb3
Nikky Rojas <rojas7313@gmail.com>
2022-06-06 17:09:21 -0600

    write method

--- lib/author.rb
+++ lib/author.rb
@@ -10,20 +10,20 @@ class Author
     name = @first_name + " " + @last_name
   end
 
-  # def write(book_name, book_publication_date)
-  #   new_book = Book.new({author_first_name: @first_name, author_last_name: @last_name,title: book_name, publication_date: book_publication_date})
-  #   # @books << new_book
-  #
-  #   # binding.pry
-  # end
-
-
-
-
   def write(book_name, book_publication_date)
-    @books.each do |book|
-      Book.new({author_first_name: @first_name, author_last_name: @last_name,title: book_name, publication_date: book_publication_date})
-      @books << book
-    end
+    new_book = Book.new({author_first_name: @first_name, author_last_name: @last_name,title: book_name, publication_date: book_publication_date})
+    # @books << new_book
+
+    # binding.pry
+  end
+
+
+
+
+  # def write(book_name, book_publication_date)
+  #   @books.each do |book|
+  #     Book.new({author_first_name: @first_name, author_last_name: @last_name,title: book_name, publication_date: book_publication_date})
+  #   end
+  # end
 
 end
