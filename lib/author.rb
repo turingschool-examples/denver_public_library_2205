52e6c1a254a09af2b9cadf536b62f352d09fc0e9
Brenna Stuart <brennacodes@gmail.com>
2022-06-06 12:27:08 -0600

    Test: Author has a name passing

--- lib/author.rb
+++ lib/author.rb
@@ -2,11 +2,27 @@ require 'helper'
 require 'helpable'
 
 class Author
+  include Helpable
   attr_reader :first_name,
-              :last_name
+              :last_name,
+              :books
 
   def initialize(details)
     @first_name = details[:first_name]
     @last_name = details[:last_name]
+    @books = []
+  end
+
+  def write(title, publication_date)
+    @books << Book.new({
+      author_first_name: @first_name,
+      author_last_name: @last_name,
+      title: title,
+      publication_date: publication_date
+    })
+  end
+
+  def name
+    @first_name + ' ' + @last_name
   end
 end
--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -2,8 +2,22 @@ require './lib/helper'
 
 RSpec.describe Author do
   let!(:charlotte_bronte) {Author.new({first_name: "Charlotte", last_name: "Bronte"})}
+  let!(:jane_eyre) {charlotte_bronte.write("Jane Eyre", "October 16, 1847")}
+
 
   it "exists" do
     expect(charlotte_bronte).to be_instance_of Author
+  end
+
+  it "has a name" do
+    expect(charlotte_bronte.name).to eq("Charlotte Bronte")
+  end
+
+  xit "has an array of books" do
+    expect(charlotte_bronte.books).to eq([])
+  end
+
+  xit "can write a book" do
+
   end
 end
