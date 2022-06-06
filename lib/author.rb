6dea12bc663a646c74fbfe3153c93125009dc066
Drew MacNicholas <dmacnicholas@gmail.com>
2022-06-05 22:36:04 -0500

    Method/Add initialize method

---
+++ lib/author.rb
@@ -0,0 +1,8 @@
+class Author
+  attr_reader :first_name, :last_name
+
+  def initialize(author_attributes)
+    @first_name = author_attributes[:first_name]
+    @last_name = author_attributes[:last_name]
+  end
+end
