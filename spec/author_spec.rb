5c163aed2ccdf6b660538a51518771f87e969005
Thiago Silveira <thiago1992@yahoo.com>
2022-06-06 12:10:17 -0600

    Refactor: Author spec has attributes

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe Author do
 
   it 'has attributes' do
     charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
-    expect(charlotte_bronte.name).to be_a("Charlotte Bronte")
-    expect(charlotte_bronte.books).to be_a([])
+    expect(charlotte_bronte.name).to eq("Charlotte Bronte")
+    expect(charlotte_bronte.books).to eq([])
   end
 end
