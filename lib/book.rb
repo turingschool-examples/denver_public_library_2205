e80a45a971ea42457582456909b4ff63631bff62
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-06-06 16:32:46 -0600

    test: added additional tests from iteration 1

--- lib/book.rb
+++ lib/book.rb
@@ -3,9 +3,9 @@ class Book
   attr_reader :author_first_name, :author_last_name, :title, :publication_date
 
   def initialize(input)
-    @author_first_name = :author_first_name
-    @author_last_name = :author_last_name
-    @title = :title
-    @publication_date = :publication_date
+    @author_first_name = input[:author_first_name]
+    @author_last_name = input[:author_last_name]
+    @title = input[:title]
+    @publication_date = input[:publication_date]
   end
 end
