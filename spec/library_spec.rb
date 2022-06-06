230e7d401c5294a2e65dd952850049425e9b5d78
Drew MacNicholas <dmacnicholas@gmail.com>
2022-06-05 23:51:32 -0500

    Test/Add in test for adding authors and books to array

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -18,7 +18,23 @@ RSpec.describe Library do
   it 'returns an empty array of books' do
     expect(@dpl.books).to eq([])
   end
+
   it 'returns an empty array of authors' do
     expect(@dpl.authors).to eq([])
   end
-end
+
+  it 'can add an authors and books to the array' do
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    professor = charlotte_bronte.write("The Professor", "1857")
+    villette = charlotte_bronte.write("Villette", "1853")
+    harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+    mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+
+    @dpl.add_author(charlotte_bronte)
+    @dpl.add_author(harper_lee)
+
+    expect(@dpl.authors).to eq [charlotte_bronte, harper_lee]
+    expect(@dpl.books).to eq [jane_eyre, professor, villette, mockingbird]
+  end
+end  
