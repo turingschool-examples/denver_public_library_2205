77e9a632a3b8dbbbc1df0ff8976f081bd343f9a5
Bryce Simonds <bsimonds14@gmail.com>
2022-06-05 19:04:42 -0600

    Debug: get Author to exist and have details

--- lib/author.rb
+++ lib/author.rb
@@ -1,1 +1,13 @@
+class Author
+attr_reader :author_full_name,
+            :first_name,
+            :last_name
 
+
+  def initialize(details)
+    @first_name = details[:first_name]
+    @last_name = details[:last_name]
+    @author_full_name = "#{details[:first_name]} " + "#{details[:last_name]}"
+  end
+
+end
--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -3,14 +3,21 @@ require './lib/author'
 
 RSpec.describe Author do
 before :each do
+
   @charlotte_bronte = Author.new({
     first_name: "Charlotte",
     last_name: "Bronte"
     })
+
   end
 
   it "exists" do
     expect(@charlotte_bronte).to be_a(Author)
+  end
+
+  it "has details" do
+    expect(@charlotte_bronte.first_name).to eq("Charlotte")
+    expect(@charlotte_bronte.last_name).to eq("Bronte")
   end
 
 end
