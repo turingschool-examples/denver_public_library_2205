1c9a1321ab970a3cd8b7811df5f3db9d1740faff
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-06-06 17:58:17 -0600

    feat: final submission, almost completed #publication_time_frame

--- lib/library.rb
+++ lib/library.rb
@@ -1,6 +1,6 @@
 class Library
 
-  attr_reader :name, :books, :authors
+  attr_reader :name, :books, :authors, :start, :end
 
   def initialize(library_name)
     @name = library_name
@@ -14,6 +14,14 @@ class Library
   end
 
   def publication_time_frame_for(given_author)
-    
+    date_range = []
+    given_author.books.each do |book|
+      date_range << book.publication_year
+    end
+    date_range.sort
+    start_end_range = []
+    :start == date_range[0]
+    :end == date.range.last
+    start_end_range = :start, :end
   end
 end
