dfed1d4f694823768fdbb8de8f78e3ff16a2d487
Ethan Nguyen <etnguyen16@gmail.com>
2022-06-05 18:59:08 -0600

    add add_author method and add_book method

--- lib/library.rb
+++ lib/library.rb
@@ -1,11 +1,21 @@
 class Library
-  attr_reader :name, :books
+  attr_reader :name, :books, :authors, :author
   def initialize(name)
     @name = name
     @books = []
+    @authors = []
+    @author = Author.new({first_name: "Charlotte",
+                          last_name: "Bronte"
+                          })
 
   end
 
+  def add_author(author)
+    @authors << author
+  end
 
+  def add_book(book)
+    @books << book
+  end
 
 end
