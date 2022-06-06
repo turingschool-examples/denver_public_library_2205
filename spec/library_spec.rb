5f8d1b1a7c63aa7ddd676c7af79f1e3e64af17b8
Bryce Simonds <bsimonds14@gmail.com>
2022-06-06 14:07:11 -0600

    Test: has publication time frame method

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -33,7 +33,28 @@ before :each do
     @dpl.add_author(harper_lee)
 
     expect(@dpl.authors).to eq([charlotte_bronte, harper_lee])
-    expect(@dpl.books).to eq([[professor, villette, mockingbird]]) #refactor
+    expect(@dpl.books).to eq([professor, villette, mockingbird]) #refactor
+  end
+
+  it "has publication time frame" do
+
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+
+    professor = charlotte_bronte.write("The Professor", "1857")
+
+    villette = charlotte_bronte.write("Villette", "1853")
+
+    harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+
+    mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+
+    @dpl.add_author(charlotte_bronte)
+
+    @dpl.add_author(harper_lee)
+
+    expect(@dpl.publication_time_frame_for(charlotte_bronte)).to eq({:start=>"1847", :end=>"1857"})
+    expect(@dpl.publication_time_frame_for(harper_lee)).to eq({:start=>"1960", :end=>"1960"})
+    expect(@dpl.authors).to eq([])
   end
 
 end
