5f847719cbd866259063b1691e2621d4a988f7bf
Thiago Silveira <thiago1992@yahoo.com>
2022-06-06 11:46:09 -0600

    Test: Book has attributes

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -9,6 +9,21 @@ RSpec.describe Book do
       title: "To Kill a Mockingbird",
       publication_date: "July 11, 1960"
     })
+
     expect(book).to be_a(Book)
+  end
+
+  it 'has attributes' do
+    book = Book.new
+    ({
+      author_first_name: "Harper",
+      author_last_name: "Lee",
+      title: "To Kill a Mockingbird",
+      publication_date: "July 11, 1960"
+    })
+
+    expect(book.title).to eq("To Kill a Mockingbird")
+    expect(book.author).to eq("Harper Lee")
+    expect(book.publication_year).to eq("1960")
   end
 end
