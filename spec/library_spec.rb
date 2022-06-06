b090780c8f608ca99528c079d38c310b80c432bd
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-06-06 16:34:47 -0700

    Test/added test for publication time frame method

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -36,4 +36,16 @@ RSpec.describe Library do
     expect(dpl.books).to eq([jane_eyre, professor, villette, mockingbird])
   end
 
+  it "can return a publication time frame hash for an author with the years of their first and last book" do
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    professor = charlotte_bronte.write("The Professor", "1857")
+    villette = charlotte_bronte.write("Villette", "1853")
+    mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+
+    dpl.add_author(charlotte_bronte)
+    dpl.add_author(harper_lee)
+
+    expect(dpl.publication_time_frame_for(charlotte_bronte)).to eq({:start=>"1847", :end=>"1857"})
+    expect(dpl.publication_time_frame_for(harper_lee)).to eq({:start=>"1960", :end=>"1960"})
+  end
 end
