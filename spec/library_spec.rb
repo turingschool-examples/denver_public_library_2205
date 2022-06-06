73f84811643a9b420b4beedc540447e9d8b1077a
Thiago Silveira <thiago1992@yahoo.com>
2022-06-06 12:38:22 -0600

    Test: Library exists

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -2,5 +2,8 @@ require './lib/library'
 require './lib/author'
 
 RSpec.describe Library do
-  
+  it 'exists' do
+    dpl = Library.new("Denver Public Library")
+    expect(dpl).to be_a(Library)
+  end
 end
