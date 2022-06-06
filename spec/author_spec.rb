d19a139e0bf354396e21f87e271ec11cb64def74
Brenna Stuart <brennacodes@gmail.com>
2022-06-06 12:28:50 -0600

    Test: Author has books passing

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -2,7 +2,7 @@ require './lib/helper'
 
 RSpec.describe Author do
   let!(:charlotte_bronte) {Author.new({first_name: "Charlotte", last_name: "Bronte"})}
-  let!(:jane_eyre) {charlotte_bronte.write("Jane Eyre", "October 16, 1847")}
+  # let(:jane_eyre) {charlotte_bronte.write("Jane Eyre", "October 16, 1847")}
 
 
   it "exists" do
@@ -13,8 +13,10 @@ RSpec.describe Author do
     expect(charlotte_bronte.name).to eq("Charlotte Bronte")
   end
 
-  xit "has an array of books" do
+  it "has an array of books" do
     expect(charlotte_bronte.books).to eq([])
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    expect(charlotte_bronte.books.count).to eq(1)
   end
 
   xit "can write a book" do
