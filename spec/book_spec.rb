d05c9357fd623d540cbf7250d263ca353fc407ab
Shirley DeCesari <shirley.decesari@gmail.com>
2022-06-05 21:20:54 -0700

    Test -Add 'exists and has attributes' test

---
+++ spec/book_spec.rb
@@ -0,0 +1,16 @@
+require './lib/book'
+require 'pry'
+
+RSpec.describe Book do
+  before :each do
+    @book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+  end
+
+  it "exists and has attributes" do
+    expect(@book).to be_a Book
+    expect(@book.title).to eq "To Kill a Mockingbird"
+    expect(@book.author).to eq "Harper Lee"
+    expect(@book.publication_year).to eq "1960"
+  end
+
+end
