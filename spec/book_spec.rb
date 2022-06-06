8072b9d72cb1c688ba1ee3e2ce76a9e89553ab3b
Nikky Rojas <rojas7313@gmail.com>
2022-06-06 06:58:56 -0600

    initial commit

---
+++ spec/book_spec.rb
@@ -0,0 +1,16 @@
+require './lib/book'
+require 'rspec'
+require 'pry'
+
+RSpec.describe Book do
+  it "Book exist" do
+    book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+    expect(book).to be_a(Book)
+  end
+  it "has attributes authors name, title, pub date" do
+    book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+    expect(book.title).to eq("To Kill a Mockingbird")
+    expect(book.author).to eq("Harper Lee")
+    expect(book.publication_year).to eq("1960")
+
+  end
