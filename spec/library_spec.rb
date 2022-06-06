c22746cc26cfffa45776531e88cdb89803724fd9
Thiago Silveira <thiago1992@yahoo.com>
2022-06-06 13:15:56 -0600

    Test: Library can assign a publication time frame for authors books

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -31,4 +31,22 @@ RSpec.describe Library do
     expect(dpl.authors).to eq([charlotte_bronte, harper_lee])
     expect(dpl.books).to eq([jane_eyre, professor, villette, mockingbird])
   end
+
+  it 'can assign a publication time frame for authors books' do
+    dpl = Library.new("Denver Public Library")
+
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    professor = charlotte_bronte.write("The Professor", "1857")
+    villette = charlotte_bronte.write("Villette", "1853")
+
+    harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+    mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+
+    dpl.add_author(charlotte_bronte)
+    dpl.add_author(harper_lee)
+
+    expect(dpl.publication_time_frame_for(charlotte_bronte)).to eq({:start=>"1847", :end=>"1857"})
+    expect(dpl.publication_time_frame_for(harper_lee)).to eq({:start=>"1960", :end=>"1960"})
+  end
 end
