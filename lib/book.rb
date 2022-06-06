cd459abaa9afca4e73e37ccde5034ff5875c5165
Mary Turpin <maryvirginiaturpin@gmail.com>
2022-06-06 01:00:12 -0600

    Iteration 1

--- lib/book.rb
+++ lib/book.rb
@@ -1,1 +1,19 @@
 class Book
+
+  attr_accessor :title, :author, :publication_date
+
+  def initialize(book)
+    @title = book[:title]
+    @author = book[:author_first_name] + " " + book[:author_last_name]
+    @publication_date = book[:publication_date]
+  end
+
+  def publication_year
+    extract = []
+    extract_r = []
+    @publication_date.each_char {|x| extract << x}
+      extract.reverse_each {|x| extract_r << x}
+      date = extract_r[0...4]
+      date = date.reverse.join("")
+  end
+end
--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -6,4 +6,12 @@ RSpec.describe Book do
 
     expect(book).to be_a(Book)
   end
+
+  it 'holds info' do
+    book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+
+    expect(book.title).to eq("To Kill a Mockingbird")
+    expect(book.author).to eq("Harper Lee")
+    expect(book.publication_year).to eq("1960")
+  end
 end
