926121640b2ad6895fd9feba60d98ece41fc29ad
Lucas Colwell <colwellc37@gmail.com>
2022-06-06 17:36:04 -0600

    Feat: add publication_time_frame method

--- lib/library.rb
+++ lib/library.rb
@@ -14,4 +14,14 @@ class Library
     author_books = author.books
     author_books.map {|book| @books << book}
   end
+
+  def publication_time_frame_for(author)
+    author_books = author.books
+    highest_year_book = author_books.max_by {|book| book.publication_year.to_i}
+    lowest_year_book = author_books.min_by {|book| book.publication_year.to_i}
+    range_hash = {}
+    range_hash[:start] = lowest_year_book.publication_year
+    range_hash[:end] = highest_year_book.publication_year
+    return range_hash
+  end
 end
