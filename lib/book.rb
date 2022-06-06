106bcf0613e773e12a36465ab877a9ed256a3da5
Nick Jones <nickjones9605@gmail.com>
2022-06-05 20:33:34 -0600

    Feature: add initialize with array from data, edit first test to cover data

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,11 @@
 class Book
 
+attr_reader :author_last_name,
+            :author_first_name,
+            :title
 def initialize(data)
-  @data = data
+  @author_last_name = data[:author_last_name]
+  @author_first_name = data[:author_first_name]
+  @title = data[:title]
 end
 end
--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -4,15 +4,18 @@ require './lib/book.rb'
 RSpec.describe Book do
 
   it "exists" do
-    book = Book.new
+    data = {author_first_name: "Harper",
+                      author_last_name: "Lee", title: "To Kill a Mockingbird",
+                      publication_date: "July 11, 1960"}
+    book = Book.new(data)
     expect(book).to be_instance_of(Book)
   end
 
   it "can return details of a book" do
-    book = Book.new({author_first_name: "Harper",
+    data = {author_first_name: "Harper",
                       author_last_name: "Lee", title: "To Kill a Mockingbird",
-                      publication_date: "July 11, 1960"})
-
+                      publication_date: "July 11, 1960"}
+    book = Book.new(data)
     expect(book.author_first_name).to eq("Harper")
     expect(book.author_last_name).to eq("Lee")
     expect(book.title).to eq("To Kill a Mockingbird")
