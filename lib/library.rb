9b1be601a523b2b7cb22b17a99e75ec774beee72
Jared Hardinger <jaredhardinger@gmail.com>
2022-06-06 12:36:50 -0400

    Add iter 4 methods

--- lib/library.rb
+++ lib/library.rb
@@ -1,9 +1,11 @@
 class Library
-  attr_reader :name, :books, :authors
+  attr_reader :name, :books, :authors, :checked_out_books
   def initialize(name)
     @name = name
     @books = []
     @authors = []
+    @checked_out_books = []
+    @book_counter = Hash.new(0)
   end
 
   def add_author(author)
@@ -18,5 +20,32 @@ class Library
     time_frame[:start] = years.sort.first
     time_frame[:end] = years.sort.last
     time_frame
+  end
+
+  def checkout(book)
+    if @books.include?(book)
+      if @checked_out_books.include?(book)
+        false
+      else
+      @checked_out_books << book
+      @book_counter[book] += 1
+      true
+      end
+    else
+      false
+    end
+  end
+
+  def return(book)
+    if @books.include?(book) && @checked_out_books.include?(book)
+      @checked_out_books.delete(book)
+      else
+      false
+    end
+  end
+
+  def most_popular_book
+    sorted_books = @book_counter.sort_by {|k,v| -v}
+    sorted_books.first.first
   end
 end
