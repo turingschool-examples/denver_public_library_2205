2adb220517f3da329314b16bccd7307008048b18
Nikky Rojas <rojas7313@gmail.com>
2022-06-06 17:07:14 -0600

    iteration 2

---
+++ lib/author.rb
@@ -0,0 +1,29 @@
+class Author
+  attr_reader :first_name, :last_name, :books, :publication_date
+  def initialize(author)
+    @first_name = author[:first_name]
+    @last_name = author[:last_name]
+    @books = []
+  end
+
+  def name
+    name = @first_name + " " + @last_name
+  end
+
+  # def write(book_name, book_publication_date)
+  #   new_book = Book.new({author_first_name: @first_name, author_last_name: @last_name,title: book_name, publication_date: book_publication_date})
+  #   # @books << new_book
+  #
+  #   # binding.pry
+  # end
+
+
+
+
+  def write(book_name, book_publication_date)
+    @books.each do |book|
+      Book.new({author_first_name: @first_name, author_last_name: @last_name,title: book_name, publication_date: book_publication_date})
+      @books << book
+    end
+
+end
--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -7,4 +7,26 @@ RSpec.describe Author do
     charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
     expect(charlotte_bronte).to be_a(Author)
   end
+
+  it "author has a full name" do
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    expect(charlotte_bronte.name).to eq("Charlotte Bronte")
+  end
+
+  it "author has an empty array of books to start" do
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    expect(charlotte_bronte.books).to eq([])
+  end
+
+  it "author can write books" do
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    villette = charlotte_bronte.write("Villette", "1853")
+
+    charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    charlotte_bronte.write("Villette", "1853")
+    expect(charlotte_bronte.write("Jane Eyre", "October 16, 1847")).to be_a Book
+
+    expect(charlotte_bronte.write("Villette", "1853")).to be_a Book
+# binding.pry
+  end
 end
