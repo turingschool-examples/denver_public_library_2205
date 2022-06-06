63f17cca9d24e186398a05cb8ee3a57a03574a9e
Shirley DeCesari <shirley.decesari@gmail.com>
2022-06-06 11:21:27 -0700

    Test -Finish iteration 3 test

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -28,14 +28,23 @@ RSpec.describe Library do
     villette = @charlotte_bronte.write("Villette", "1853")
     mockingbird = @harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
 
-    @charlotte_bronte.add_books(jane_eyre)
-    @charlotte_bronte.add_books(professor)
-    @charlotte_bronte.add_books(villette)
-    @harper_lee.add_books(mockingbird)
-
     @dpl.add_author(@charlotte_bronte)
     @dpl.add_author(@harper_lee)
 
     expect(@dpl.books).to eq [jane_eyre, professor, villette, mockingbird]
+  end
+
+  it "can return publication time fram for an author" do
+    jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    professor = @charlotte_bronte.write("The Professor", "1857")
+    villette = @charlotte_bronte.write("Villette", "1853")
+    mockingbird = @harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+
+
+    @dpl.add_author(@charlotte_bronte)
+    @dpl.add_author(@harper_lee)
+
+    expect(@dpl.publication_time_frame_for(@charlotte_bronte)).to eq ({:start=>"1847", :end=>"1857"})
+    expect(@dpl.publication_time_frame_for(@harper_lee)).to eq ({:start=>"1960", :end=>"1960"})
   end
 end
