d9abdc4a75671066beb775865c80b70aa0a6583d
Lucas Colwell <colwellc37@gmail.com>
2022-06-06 16:36:32 -0600

    Test: create tests for author class/it2

---
+++ spec/author_spec.rb
@@ -0,0 +1,33 @@
+require 'rspec'
+require './lib/class'
+require './lib/author'
+
+RSpec.describe Author do
+  describe 'Iteration 2' do
+    before :each do
+      @charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    end
+
+    it 'exists' do
+      expect(@charlotte_bronte).to be_a(Author)
+      expect(@jane_eyre).to be_a(Author)
+    end
+
+    it 'has attributes' do
+      expect(@class.name).to eq("Charlotte Bronte")
+      expect(@class.books).to eq([])
+    end
+
+    it "can write a book" do
+      jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+
+      expect(jane_eyre.class).to be_a(Book)
+      expect(jane_eyre.title).to eq("Jane Eyre")
+      expect(@charlotte_bronte.books).to eq([jane_eyre])
+
+      villette = @charlotte_bronte.write("Villette", "1853")
+
+      expect(@charlotte_bronte.books).to eq([jane_eyre, villette])
+    end
+  end
+end
