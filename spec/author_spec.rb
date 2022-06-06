7d7420e4a89d5504a5cc441f729ce7f01c60509e
Bryce Simonds <bsimonds14@gmail.com>
2022-06-06 13:31:31 -0600

    Added library rb and spec files

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -23,7 +23,6 @@ before :each do
 
   it "can add books to the books array attribute" do
     @jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
-    # require 'pry';binding.pry
     expect(@jane_eyre).to be_a(Book)
     expect(@jane_eyre.title).to eq("Jane Eyre")
     expect(@charlotte_bronte.books).to eq([@jane_eyre])
