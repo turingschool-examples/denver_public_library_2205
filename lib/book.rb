40d24c3ffedc3cd3047f74c96041156861a54c41
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-06-06 16:42:03 -0600

    feat: completed iteration 1 method + test

--- lib/book.rb
+++ lib/book.rb
@@ -8,4 +8,15 @@ class Book
     @title = input[:title]
     @publication_date = input[:publication_date]
   end
+
+  def publication_year
+    @publication_date
+    temp_array = @publication_date.split
+    year_yanker = temp_array[2]
+    # require 'pry'; binding.pry
+  end
+
+  def author
+    @author_first_name + " " + @author_last_name
+  end
 end
--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -2,22 +2,22 @@ require './lib/book'
 
 RSpec.describe Book do
   before :each do
-    book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+    @book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
   end
 
   it "exists" do
-    expect(book).to be_instance_of(Book)
+    expect(@book).to be_instance_of(Book)
   end
 
   it "returns the book title" do
-    expect(book.title).to eq("To Kill a Mockingbird")
+    expect(@book.title).to eq("To Kill a Mockingbird")
   end
 
   it "returns the book author" do
-    expect(book.author).to eq("Harper Lee")
+    expect(@book.author).to eq("Harper Lee")
   end
 
   it "returns the book publication year" do
-    expect(book.publication_year).to eq("1960")
+    expect(@book.publication_year).to eq("1960")
   end
 end
