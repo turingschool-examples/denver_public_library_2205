91230f4f1979601e202ec32995acf536373df951
Bryce Simonds <bsimonds14@gmail.com>
2022-06-06 13:33:23 -0600

    Test: it exists test

---
+++ spec/library_spec.rb
@@ -0,0 +1,13 @@
+require './lib/book'
+require './lib/author'
+
+RSpec.describe Library do
+before :each do
+  @dpl = Library.new("Denver Public Library")
+  end
+
+  it "exists" do
+    expect(@dpl).to be_a(Library)
+  end
+
+end
