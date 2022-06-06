8824420a54ae670d9127df71e511ee2c596a9316
Thiago Silveira <thiago1992@yahoo.com>
2022-06-06 12:06:48 -0600

    Test: Author has attributes

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe Author do
     charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
     expect(charlotte_bronte).to be_a(Author)
   end
+
+  it 'has attributes' do
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    expect(charlotte_bronte.name).to be_a("Charlotte Bronte")
+    expect(charlotte_bronte.books).to be_a([])
+  end
 end
