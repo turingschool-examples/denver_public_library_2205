72b24efdd19cd00fdf6b73c33a66a3f0048b07db
Bryce Simonds <bsimonds14@gmail.com>
2022-06-05 18:36:49 -0600

    Debug: changed publication year to date in test has details

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,14 +1,24 @@
 require './lib/book'
 
 RSpec.describe Book do
-
-  it "exists" do
-  book = Book.new({
+before :each do
+  @book = Book.new({
     author_first_name: "Harper",
     author_last_name: "Lee",
     title: "To Kill a Mockingbird",
     publication_date: "July 11, 1960"
     })
-  expect(book).to be_a(Book)
   end
-end 
+
+  it "exists" do
+    expect(@book).to be_a(Book)
+  end
+
+  it "has details" do
+    expect(@book.title).to eq("To Kill a Mockingbird")
+    expect(@book.publication_date).to eq("July 11, 1960")
+    expect(@book.author_first_name).to eq("Harper")
+    expect(@book.author_last_name).to eq("Lee")
+    expect(@book.author).to eq("Harper Lee")
+  end
+end
