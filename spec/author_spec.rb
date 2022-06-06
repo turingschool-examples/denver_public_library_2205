b63ca0442416bae81678140b716893a91d64de98
Thiago Silveira <thiago1992@yahoo.com>
2022-06-06 12:04:31 -0600

    Test: Author exists

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -2,5 +2,8 @@ require './lib/book'
 require './lib/author'
 
 RSpec.describe Author do
-  
+  it 'exists' do
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    expect(charlotte_bronte).to be_a(Author)
+  end
 end
