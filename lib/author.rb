a32110df0cb933de3a1f99730a20b22775396cc7
Drew MacNicholas <dmacnicholas@gmail.com>
2022-06-05 22:43:05 -0500

    Method/Add books by author method

--- lib/author.rb
+++ lib/author.rb
@@ -1,9 +1,10 @@
 class Author
-  attr_reader :first_name, :last_name
+  attr_reader :first_name, :last_name, :books
 
   def initialize(author_attributes)
     @first_name = author_attributes[:first_name]
     @last_name = author_attributes[:last_name]
+    @books = []
   end
 
   def name
