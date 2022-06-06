616f803f4911c3d5fd110321cb2178f26577e60c
Thiago Silveira <thiago1992@yahoo.com>
2022-06-06 12:54:42 -0600

    Test: Library can add books that authors write and return book/author collections

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -13,4 +13,22 @@ RSpec.describe Library do
     expect(dpl.books).to eq([])
     expect(dpl.authors).to eq([])
   end
+
+  it 'can add books that authors write and return book/author collections' do
+    dpl = Library.new("Denver Public Library")
+
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    professor = charlotte_bronte.write("The Professor", "1857")
+    villette = charlotte_bronte.write("Villette", "1853")
+
+    harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+    mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+
+    dpl.add_author(charlotte_bronte)
+    dpl.add_author(harper_lee)
+
+    expect(dpl.authors).to eq([charlotte_bronte, harper_lee])
+    expect(dpl.books).to eq([jane_eyre, professor, villette, mockingbird])
+  end
 end
