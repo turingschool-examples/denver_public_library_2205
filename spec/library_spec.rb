08c13ebfa26a646b80521cf581e9797e1af16740
Bryce Simonds <bsimonds14@gmail.com>
2022-06-06 13:44:47 -0600

    Test: can add authors with books

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -17,4 +17,23 @@ before :each do
     expect(@dpl.authors).to eq([])
   end
 
+  it "can add authors that have written books" do
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+
+    professor = charlotte_bronte.write("The Professor", "1857")
+
+    villette = charlotte_bronte.write("Villette", "1853")
+
+    harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+
+    mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+
+    @dpl.add_author(charlotte_bronte)
+
+    @dpl.add_author(harper_lee)
+
+    expect(@dpl.authors).to eq(charlotte_bronte, harper_lee)
+    expect(@dpl.books).to eq([professor, villette, mockingbird])
+  end
+
 end
