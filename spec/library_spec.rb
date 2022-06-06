cda0d108325abe1ea5e2f0259b2a041b0f7417d4
Drew MacNicholas <dmacnicholas@gmail.com>
2022-06-05 23:39:03 -0500

    Test/Add test for empty array of books

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -14,4 +14,8 @@ RSpec.describe Library do
   it 'has a name' do
     expect(@dpl.name).to eq("Denver Public Library")
   end
+
+  it 'returns an empty array of books' do
+    expect(@dpl.books).to eq([])
+  end
 end
